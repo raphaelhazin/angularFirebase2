eb077a159d62d22b8058d800b18546a67c189f26
RaphaelHazin <raphael@incorptech.com.br>
2021-04-21 14:31:29 -0300

    Ajustado com classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import firebase from "firebase/app";
-import "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB-EObrp2hmcnMCiPBzn3uUxnr69XAK_-0",
-  authDomain: "angularspike-672db.firebaseapp.com",
-  databaseURL: "https://angularspike-672db-default-rtdb.firebaseio.com",
-  projectId: "angularspike-672db",
-  storageBucket: "angularspike-672db.appspot.com",
-  messagingSenderId: "229448386006",
-  appId: "1:229448386006:web:3e54a76042a06077f81077",
-  measurementId: "G-2762CJWVVJ"
-};
-
+import {Firebase} from './firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,14 +7,12 @@ const firebaseConfig = {
 })
 export class AppComponent {
   
+  
   ngOnInit(): void {
-    var dbRealTime = firebase.initializeApp(firebaseConfig); 
-    const db = dbRealTime.database().ref('chave/' + 1000);
-    db.set({
-      nome:"rafa",
-      idade:10
-    });
-
+    let db = new Firebase();
+    db.conectar();
+    db.salvar();
+    db.consultar();
   }
 
   title = 'estudarFirebase';
--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,2 +1,47 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
 export class Firebase {
+
+    firebaseConfig;
+    instancia;
+
+    constructor() {
+        this.firebaseConfig =  {
+            apiKey: "AIzaSyB-EObrp2hmcnMCiPBzn3uUxnr69XAK_-0",
+            authDomain: "angularspike-672db.firebaseapp.com",
+            databaseURL: "https://angularspike-672db-default-rtdb.firebaseio.com",
+            projectId: "angularspike-672db",
+            storageBucket: "angularspike-672db.appspot.com",
+            messagingSenderId: "229448386006",
+            appId: "1:229448386006:web:3e54a76042a06077f81077",
+            measurementId: "G-2762CJWVVJ"
+          };
+          
+    }
+
+    conectar() {
+        this.instancia = firebase.initializeApp(this.firebaseConfig); 
+    }
+
+    salvar() {
+        var dado = {
+            nome:"rafa",
+            idade:10
+          };
+        const db = this.instancia.database().ref('chave/' + 1000);
+        db.set(dado);
+    
+    }
+
+    consultar() {
+        var dado = firebase.database().ref('chave/' + 1000);
+
+        dado.on('value', (snapshot) => {
+            const data = snapshot.val;
+            console.log("Peneirinha", data);
+        })
+
+    
+    }    
 }
