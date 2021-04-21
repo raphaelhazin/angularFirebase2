28374c036db3a3d8bdec8e64651b8b03cceb44fa
RaphaelHazin <raphael@incorptech.com.br>
2021-04-21 13:09:49 -0300

    Adicionados os Arquivos

---
+++ src/app/cadastro-pessoa/cadastro-pessoa.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-cadastro-pessoa',
+  templateUrl: './cadastro-pessoa.component.html',
+  styleUrls: ['./cadastro-pessoa.component.css']
+})
+export class CadastroPessoaComponent implements OnInit {
+
+  pessoas:Array<any>;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.pessoas = new Array<any>();
+  }
+
+  form_submit(f: NgForm) {
+    this.pessoas.push({ nome: f.form.controls.nome.value, sobrenome: f.form.controls.sobrenome.value });
+    console.log(this.pessoas);
+    }
+
+}
